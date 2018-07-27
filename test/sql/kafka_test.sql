c48a81a9dadbb5b7f3c9b7cbbcbe3c264209476d
Ildar Musin <zildermann@gmail.com>
2018-07-27 15:15:51 +0200

    test refactor

--- test/sql/kafka_test.sql
+++ test/sql/kafka_test.sql
@@ -126,30 +126,3 @@ PREPARE kafka_test(int,bigint,bigint, int,bigint,bigint) AS SELECT COUNT(*) FROM
 EXPLAIN (COSTS OFF) EXECUTE kafka_test(1,100,200,1,500,600);
 EXPLAIN (COSTS OFF) EXECUTE kafka_test(1,1,2,1,5,6);
 
-
--- test ANALYZE
-CREATE FOREIGN TABLE kafka_test_json (
-    part int OPTIONS (partition 'true'),
-    offs bigint OPTIONS (offset 'true'),
-    some_int int OPTIONS (json 'int_val'),
-    some_text text OPTIONS (json 'text_val'),
-    some_date date OPTIONS (json 'date_val'),
-    some_time timestamp OPTIONS (json 'time_val')
-)
-SERVER kafka_server OPTIONS
-    (format 'json', topic 'contrib_regress_json', batch_size '30', buffer_delay '500');
--- set default costs so that local settings won't affect the test
-SET seq_page_cost     = 1.0;
-SET cpu_tuple_cost    = 0.01;
-SET cpu_operator_cost = 0.0025;
-SET max_parallel_workers_per_gather = 0;
--- without statistics
-EXPLAIN SELECT FROM kafka_test_json WHERE some_int < 50;
-EXPLAIN SELECT FROM kafka_test_json WHERE some_int < 500;
-EXPLAIN SELECT FROM kafka_test_json WHERE some_int > 50000;
--- with statistics
-ANALYZE kafka_test_json;
-EXPLAIN SELECT FROM kafka_test_json WHERE some_int < 50;
-EXPLAIN SELECT FROM kafka_test_json WHERE some_int < 500;
-EXPLAIN SELECT FROM kafka_test_json WHERE some_int > 50000;
-DROP FOREIGN TABLE kafka_test_json;
